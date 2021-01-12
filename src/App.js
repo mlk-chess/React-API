b5baa458bc98134c7cb22ad9208b828af7dd85a7
mlk-chess <m.kajeiou123@gmail.com>
2021-01-12 23:16:33 +0100

    Signed commit x2

--- src/App.js
+++ src/App.js
@@ -24,9 +24,6 @@ function App() {
                 <Header_content/>
             </header>
 
-            <Route path={"/test"}>
-                <h1>Test</h1>
-            </Route>
             {/*Présentation de la première API: DOFUS*/}
             <Route path="/Pres1">
                 <h1> Monstres Choisis </h1>
